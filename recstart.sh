469d72519b2d568e67df46f081c0270afd5d1b19
ijs01140 <ijs01140.1@gmail.com>
2019-03-01 19:16:16 +0900

    sepalate post command

--- recstart.sh
+++ recstart.sh
@@ -5,4 +5,4 @@ source "$script_dir/privenv.sh"
 DATESTART=`date --date "@$STARTAT"`
 DATEEND=`date --date "@$ENDAT"`
 POSTTEXT="*録画開始*\n番組名:$NAME\n開始:$DATESTART"
-curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"$POSTTEXT\"}" $SLACKURL
+source "$script_dir/post2slack.sh"
