0b9f276a895e8039bed7adcb22d1451deb8a3789
ijs01140 <ijs01140.1@gmail.com>
2019-03-01 19:07:08 +0900

    absolute path

--- recstart.sh
+++ recstart.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-source privenv.sh
+script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
+source "$script_dir/privenv.sh"
 DATESTART=`date --date "@$STARTAT"`
 DATEEND=`date --date "@$ENDAT"`
 POSTTEXT="*録画開始*\n番組名:$NAME\n開始:$DATESTART"
