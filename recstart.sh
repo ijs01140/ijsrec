4b77623f3e469a4a801e2b769ed0b61db9409fff
ijs01140 <ijs01140.1@gmail.com>
2019-03-01 20:52:02 +0900

    change posttext format

--- recstart.sh
+++ recstart.sh
@@ -4,5 +4,5 @@ script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 source "$script_dir/privenv.sh"
 DATESTART=`date --date "@$STARTAT"`
 DATEEND=`date --date "@$ENDAT"`
-POSTTEXT="*録画開始*\n番組名:$NAME\n開始:$DATESTART"
+POSTTEXT="*録画開始*\n番組名:$NAME\n放送時間:$DATESTART - $DATEEND"
 source "$script_dir/post2slack.sh"
