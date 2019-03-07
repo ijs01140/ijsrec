b061e198af5da95420cd094ec4f6338cf057e059
ijs01140 <ijs01140.1@gmail.com>
2019-03-07 15:55:28 +0900

    convert msec to sec

--- recstart.sh
+++ recstart.sh
@@ -2,7 +2,12 @@
 
 script_dir=$(cd $(dirname $BASH_SOURCE); pwd)
 source "$script_dir/privenv.sh"
-DATESTART=`date --date "@$STARTAT"`
-DATEEND=`date --date "@$ENDAT"`
+#convert unixtime[ms] to unixtime[s]
+STARTATSEC=$(($STARTAT/1000))
+ENDATSEC=$(($ENDAT/1000))
+#convert unixtime to date
+DATESTART=`date --date "@$STARTATSEC"`
+DATEEND=`date --date "@$ENDATSEC"`
+#post to slack
 POSTTEXT="*録画開始*\n番組名:$NAME\n放送時間:$DATESTART - $DATEEND"
 source "$script_dir/post2slack.sh"
